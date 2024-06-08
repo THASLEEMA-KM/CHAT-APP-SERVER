83ca8adae4774b9214531a80f08ba39d927d3bc2
THASLEEMA-KM <thasleemakm5@gmail.com>
2024-06-08 20:17:58 +0530

    Chat App Server Completed

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const http = require('http')
+const socketIo = require("socket.io")
+const cors = require('cors');
+const helmet = require('helmet');
+
+const app = express()
+const server = http.createServer(app)
+
+const io = socketIo(server , {
+    cors:{
+        origin:"http://localhost:5173",
+        methods:['GET','POST']
+    }
+})
+
+app.get("/",(req,res)=>{
+    res.send("working!!!")
+  })
+
+
+app.use(cors());
+app.use(helmet({
+    contentSecurityPolicy: {
+        directives: {
+          defaultSrc: ["'self'"],
+          scriptSrc: ["'self'", "'unsafe-inline'"],
+          connectSrc: ["'self'", "http://localhost:5173"],
+          styleSrc: ["'self'", "'unsafe-inline'"],
+          fontSrc: ["'self'", "data:"]
+        },
+      },
+}))
+
+io.on("connection" , (socket)=>
+{
+    console.log('User connected');
+
+    socket.on("chat message" , (msg)=>
+        {
+            io.emit('chat message' , msg);
+        }
+    );
+
+    socket.on('disconnect' , ()=>
+    {
+        console.log('User disconnected');
+    })
+
+});
+
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+    console.log(`Server running on port  http://localhost:${PORT}`);
+}
+);
